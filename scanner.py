8d2727dff3f78f65e27835e9cb12ba583baf3f4d
Rushi Ranpise <136841749+rushizgithub@users.noreply.github.com>
2023-07-21 12:05:10 +0530

    [skip ci]

---
+++ scanner.py
@@ -0,0 +1,30 @@
+import os
+import re
+import sys
+
+def get_python_version(file_path):
+    with open(file_path, "r") as file:
+        content = file.read()
+
+        # Use regular expression to find the Python shebang or version comment
+        match = re.search(r"(#!/usr/bin/env\s+python(\d+(\.\d+)*)?|python(\d+(\.\d+)*)?)", content)
+
+        if match:
+            return match.group().replace("#!", "").strip()
+
+    return None
+
+def process_folder(folder_path):
+    for root, _, files in os.walk(folder_path):
+        for file_name in files:
+            file_path = os.path.join(root, file_name)
+            python_version = get_python_version(file_path)
+
+            if python_version:
+                print(f"File: {file_path}, Python version: {python_version}")
+            else:
+                print(f"File: {file_path}, Python version not specified")
+
+if __name__ == "__main__":
+    folder_path = "util"  # Replace with the actual path to your folder
+    process_folder(folder_path)
